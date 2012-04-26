bc067c5b95caba772106ccff41dfda78b891f310
Reinis Ivanovs <dabas@untu.ms>
2012-04-26 18:00:42 +0300

    Add names to server config

--- config/server.py
+++ config/server.py
@@ -1,17 +1,21 @@
 logger = None
 ttl = 90
 wan_ip = None
+names = {}
 
 
 def _update_config():
     import logging
+    import json
     from os import getenv
 
-    global logger, wan_ip
+    global logger, wan_ip, names
 
     logging.basicConfig(level=logging.INFO)
     logger = logging.getLogger()
     wan_ip = getenv('SSH_CONNECTION').split(' ')[0]
+
+    names = json.loads(getenv('SSH_ORIGINAL_COMMAND'))
 
 
 _update_config()
